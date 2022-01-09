65f4996f7507ef01950fa4c6220e29714c9d05cc
Siddharth <siddharthnema04@gmail.com>
2022-01-09 18:26:25 -0500

    fixed routing issues

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import { useUserAuth } from "../context/UserAuthContext";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, Navigate } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import { Form, Alert } from "react-bootstrap";
 import GoogleButton from "react-google-button";
 
-const Login = () => {
+const Login = ({ children }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  const { logIn, googleSignIn } = useUserAuth();
+  const { logIn, googleSignIn, user } = useUserAuth();
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -35,36 +35,40 @@ const Login = () => {
     }
   };
 
-  return (
-    <>
-      <div className="p-4 box">
-        <h2 className="mb-3">Volunteer Login</h2>
-        {error && <Alert variant="danger">{error}</Alert>}
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Control type="email" placeholder="Email address" onChange={(e) => setEmail(e.target.value)} />
-          </Form.Group>
+  if (!user) {
+    return (
+      <>
+        <div className="p-4 box">
+          <h2 className="mb-3">Volunteer Login</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Control type="email" placeholder="Email address" onChange={(e) => setEmail(e.target.value)} />
+            </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-          </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+            </Form.Group>
 
-          <div className="d-grid gap-2">
-            <Button variant="primary" type="Submit">
-              Log In
-            </Button>
+            <div className="d-grid gap-2">
+              <Button variant="primary" type="Submit">
+                Log In
+              </Button>
+            </div>
+          </Form>
+          <hr />
+          <div>
+            <GoogleButton className="g-btn" type="dark" onClick={handleGoogleSignIn} />
           </div>
-        </Form>
-        <hr />
-        <div>
-          <GoogleButton className="g-btn" type="dark" onClick={handleGoogleSignIn} />
         </div>
-      </div>
-      <div className="p-4 box mt-3 text-center">
-        Don't have an account? <Link to="/signup">Sign up</Link>
-      </div>
-    </>
-  );
+        <div className="p-4 box mt-3 text-center">
+          Don't have an account? <Link to="/signup">Sign up</Link>
+        </div>
+      </>
+    );
+  } else {
+    return <Navigate to="/dashboard" />;
+  }
 };
 
 export default Login;
